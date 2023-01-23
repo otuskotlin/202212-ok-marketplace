1902a8bbcae4dd70d2e56812b39a100a2000c28c
tano <tanoshkin@ya.ru>
2023-01-23 23:15:23 +0300

    Add KMP part.

---
+++ m1l7-kmp/build.gradle.kts
@@ -0,0 +1,81 @@
+plugins {
+    kotlin("multiplatform")
+}
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "1.8"
+        }
+        withJava()
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+    }
+
+    js(IR) {
+        browser {}
+    }
+
+    val hostOs = System.getProperty("os.name")
+    val isMingwX64 = hostOs.startsWith("Windows")
+    val nativeTarget = when {
+        hostOs == "Mac OS X" -> macosArm64("native")
+        hostOs == "Linux" -> linuxX64("native")
+        isMingwX64 -> mingwX64("native")
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+
+    nativeTarget.apply {
+        compilations.getByName("main") {
+            cinterops {
+                val libcurl by creating
+            }
+        }
+        binaries {
+            executable {
+                entryPoint = "main"
+            }
+        }
+    }
+
+    val coroutinesVersion: String by project
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutinesVersion")
+            }
+        }
+        val jvmMain by getting
+        val jvmTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+        val jsMain by getting {
+            dependencies {
+                implementation(npm("js-big-decimal", "~1.3.4"))
+                implementation(npm("is-sorted", "~1.0.5"))
+            }
+        }
+        val jsTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+        val nativeMain by getting
+        val nativeTest by getting
+    }
+}
---
+++ m1l7-kmp/src/nativeMain/kotlin/CommonApiService.kt
@@ -0,0 +1,5 @@
+actual class CommonApiService actual constructor() {
+    actual suspend fun makeCall(): String {
+        TODO("Not yet implemented")
+    }
+}
