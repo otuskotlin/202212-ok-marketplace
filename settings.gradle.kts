037a9f5e14e0c18bc5e36289a4a92fe254bb36b6
Sergey Okatov <sokatov@gmail.com>
2023-02-08 14:57:55 +0500

    m2l3 Kotest

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,21 @@
 rootProject.name = "ok-marketplace-202212"
 
+pluginManagement {
+    val kotlinVersion: String by settings
+    val kotestVersion: String by settings
+
+    plugins {
+        kotlin("jvm") version kotlinVersion
+        kotlin("multiplatform") version kotlinVersion
+        id("io.kotest.multiplatform") version kotestVersion apply false
+    }
+}
+
+
 include("m1l1-quickstart")
+include("m1l3-oop")
+include("m1l4-dsl")
+include("m1l5-coroutines")
+include("m1l6-flows-and-channels")
+include("m1l7-kmp")
+include("m2l3-testing")
