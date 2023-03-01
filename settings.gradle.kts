3dda9596b4b3f291fe720eca9ff8631c0d271ef3
BarracudaPff <mrm.kikll@gmail.com>
2023-03-01 04:21:21 +0100

    [m3l1-spring] add spring dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,10 @@ pluginManagement {
     val kotlinVersion: String by settings
     val kotestVersion: String by settings
     val openapiVersion: String by settings
+    val springframeworkBootVersion: String by settings
+    val springDependencyManagementVersion: String by settings
+    val pluginSpringVersion: String by settings
+    val pluginJpa: String by settings
 
     plugins {
         kotlin("jvm") version kotlinVersion
@@ -12,6 +16,11 @@ pluginManagement {
         kotlin("plugin.serialization") version kotlinVersion apply false
 
         id("org.openapi.generator") version openapiVersion apply false
+
+        id("org.springframework.boot") version springframeworkBootVersion apply false
+        id("io.spring.dependency-management") version springDependencyManagementVersion apply false
+        kotlin("plugin.spring") version pluginSpringVersion apply false
+        kotlin("plugin.jpa") version pluginJpa apply false
     }
 }
 
@@ -30,3 +39,6 @@ include("ok-marketplace-api-v2-kmp")
 include("ok-marketplace-common")
 include("ok-marketplace-mappers-v1")
 include("ok-marketplace-mappers-v2")
+
+include("ok-marketplace-stubs")
+include("ok-marketplace-app-spring")
