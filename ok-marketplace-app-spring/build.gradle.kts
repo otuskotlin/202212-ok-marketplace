a29870c52da14986b49db6bdc2d290f8fa0caa2c
BarracudaPff <mrm.kikll@gmail.com>
2023-03-03 16:53:33 +0100

    [m3l1-spring] temp disable spring websocket

--- ok-marketplace-app-spring/build.gradle.kts
+++ ok-marketplace-app-spring/build.gradle.kts
@@ -12,7 +12,7 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-actuator") // info; refresh; springMvc output
     implementation("org.springframework.boot:spring-boot-starter-web") // Controller, Service, etc..
-    implementation("org.springframework.boot:spring-boot-starter-websocket") // Controller, Service, etc..
+    // implementation("org.springframework.boot:spring-boot-starter-websocket") // Controller, Service, etc..
     implementation("org.springdoc:springdoc-openapi-ui:$springdocOpenapiUiVersion")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin") // from models to json and Vice versa
     implementation("org.jetbrains.kotlin:kotlin-reflect") // for spring-boot app
