9b1f1be16a68d346e0bb09992a02e173c1ebabd6
BarracudaPff <mrm.kikll@gmail.com>
2023-03-15 00:19:27 +0100

    [m3l4-websocket] add and fix base mp structure

---
+++ ok-marketplace-common/src/commonMain/kotlin/helpers/MkplErrorsHelpers.kt
@@ -0,0 +1,18 @@
+package ru.otus.otuskotlin.marketplace.common.helpers
+
+import ru.otus.otuskotlin.marketplace.common.MkplContext
+import ru.otus.otuskotlin.marketplace.common.models.MkplError
+
+fun Throwable.asMkplError(
+    code: String = "unknown",
+    group: String = "exceptions",
+    message: String = this.message ?: "",
+) = MkplError(
+    code = code,
+    group = group,
+    field = "",
+    message = message,
+    exception = this,
+)
+
+fun MkplContext.addError(vararg error: MkplError) = errors.addAll(error)
