80dd8dfad37b337a477af8cc343e69c8b98961ec
BarracudaPff <mrm.kikll@gmail.com>
2023-03-15 00:20:15 +0100

    [m3l4-websocket] add v1, v2 ws controllers for ktor app

---
+++ ok-marketplace-app-ktor/src/jvmTest/kotlin/ru/otus/otuskotlin/marketplace/stubs/V2WebsocketStubTest.kt
@@ -0,0 +1,152 @@
+package ru.otus.otuskotlin.marketplace.stubs
+
+import io.ktor.client.plugins.websocket.*
+import io.ktor.server.testing.*
+import io.ktor.websocket.*
+import kotlinx.coroutines.withTimeout
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import ru.otus.otuskotlin.marketplace.api.v2.apiV2Mapper
+import ru.otus.otuskotlin.marketplace.api.v2.models.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertIs
+
+class V2WebsocketStubTest {
+
+    @Test
+    fun createStub() {
+        val request = AdCreateRequest(
+            requestId = "12345",
+            ad = AdCreateObject(
+                title = "Болт",
+                description = "КРУТЕЙШИЙ",
+                adType = DealSide.DEMAND,
+                visibility = AdVisibility.PUBLIC,
+            ),
+            debug = AdDebug(
+                mode = AdRequestDebugMode.STUB,
+                stub = AdRequestDebugStubs.SUCCESS
+            )
+        )
+
+        testMethod<IResponse>(request) {
+            assertEquals("12345", it.requestId)
+        }
+    }
+
+    @Test
+    fun readStub() {
+        val request = AdReadRequest(
+            requestId = "12345",
+            ad = AdReadObject("666"),
+            debug = AdDebug(
+                mode = AdRequestDebugMode.STUB,
+                stub = AdRequestDebugStubs.SUCCESS
+            )
+        )
+
+        testMethod<IResponse>(request) {
+            assertEquals("12345", it.requestId)
+        }
+    }
+
+    @Test
+    fun updateStub() {
+        val request = AdUpdateRequest(
+            requestId = "12345",
+            ad = AdUpdateObject(
+                id = "666",
+                title = "Болт",
+                description = "КРУТЕЙШИЙ",
+                adType = DealSide.DEMAND,
+                visibility = AdVisibility.PUBLIC,
+            ),
+            debug = AdDebug(
+                mode = AdRequestDebugMode.STUB,
+                stub = AdRequestDebugStubs.SUCCESS
+            )
+        )
+
+        testMethod<IResponse>(request) {
+            assertEquals("12345", it.requestId)
+        }
+    }
+
+    @Test
+    fun deleteStub() {
+        val request = AdDeleteRequest(
+            requestId = "12345",
+            ad = AdDeleteObject(
+                id = "666",
+            ),
+            debug = AdDebug(
+                mode = AdRequestDebugMode.STUB,
+                stub = AdRequestDebugStubs.SUCCESS
+            )
+        )
+
+        testMethod<IResponse>(request) {
+            assertEquals("12345", it.requestId)
+        }
+    }
+
+    @Test
+    fun searchStub() {
+        val request = AdSearchRequest(
+            requestId = "12345",
+            adFilter = AdSearchFilter(),
+            debug = AdDebug(
+                mode = AdRequestDebugMode.STUB,
+                stub = AdRequestDebugStubs.SUCCESS
+            )
+        )
+
+        testMethod<IResponse>(request) {
+            assertEquals("12345", it.requestId)
+        }
+    }
+
+    @Test
+    fun offersStub() {
+        val request = AdOffersRequest(
+            requestId = "12345",
+            ad = AdReadObject(
+                id = "666",
+            ),
+            debug = AdDebug(
+                mode = AdRequestDebugMode.STUB,
+                stub = AdRequestDebugStubs.SUCCESS
+            )
+        )
+
+        testMethod<IResponse>(request) {
+            assertEquals("12345", it.requestId)
+        }
+    }
+
+    private inline fun <reified T> testMethod(
+        request: IRequest,
+        crossinline assertBlock: (T) -> Unit
+    ) = testApplication {
+        val client = createClient {
+            install(WebSockets)
+        }
+
+        client.webSocket("/ws/v2") {
+            withTimeout(3000) {
+                val incame = incoming.receive() as Frame.Text
+                val response = apiV2Mapper.decodeFromString<T>(incame.readText())
+                assertIs<AdInitResponse>(response)
+            }
+            send(Frame.Text(apiV2Mapper.encodeToString(request)))
+            withTimeout(3000) {
+                val incame = incoming.receive() as Frame.Text
+                val text = incame.readText()
+                val response = apiV2Mapper.decodeFromString<T>(text)
+
+                assertBlock(response)
+            }
+        }
+    }
+}
