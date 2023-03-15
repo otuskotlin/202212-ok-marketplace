1276168d4c833252e73f94e2bec62e66ae33e85a
tano <tanoshkin@ya.ru>
2023-03-15 18:58:44 +0300

    m3l5.

---
+++ ok-marketplace-app-rabbit/src/main/kotlin/ru/otus/otuskotlin/marketplace/app/rabbit/biz/MkplAdProcessor.kt
@@ -0,0 +1,7 @@
+package ru.otus.otuskotlin.marketplace.app.rabbit.biz
+
+//  Это класс - заглушка, после вебинара по бизнес-логике от него можно будет избавиться
+class MkplAdProcessor {
+
+    fun exec() = ""
+}
