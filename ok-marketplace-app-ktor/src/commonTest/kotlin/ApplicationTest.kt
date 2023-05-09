a7f0a869161e976596f24643683aba9f02dbca76
BarracudaPff <mrm.kikll@gmail.com>
2023-05-10 00:53:51 +0200

    [m5l4-cassandra] cherry-pick ktor fixes from m5l5

--- ok-marketplace-app-ktor/src/commonTest/kotlin/ApplicationTest.kt
+++ ok-marketplace-app-ktor/src/commonTest/kotlin/ApplicationTest.kt
@@ -3,13 +3,14 @@ package ru.otus.otuskotlin.marketplace.app
 import io.ktor.client.request.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
+import ru.otus.otuskotlin.marketplace.app.helpers.testSettings
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class ApplicationTest {
     @Test
     fun `root endpoint`() = testApplication {
-        application { module() }
+        application { module(testSettings()) }
         val response = client.get("/")
         assertEquals(HttpStatusCode.NotFound, response.status)
     }
